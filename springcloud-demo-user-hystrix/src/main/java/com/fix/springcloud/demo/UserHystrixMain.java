cacb62ea70ed0f86cfe92df9993e0eb1fa354d55
francis785 <1186580085@qq.com>
2019-07-13 19:23:23 +0800

    Spring Cloud Hystrix DashBoard使用

--- springcloud-demo-user-hystrix/src/main/java/com/fix/springcloud/demo/UserHystrixMain.java
+++ springcloud-demo-user-hystrix/src/main/java/com/fix/springcloud/demo/UserHystrixMain.java
@@ -1,7 +1,9 @@
 package com.fix.springcloud.demo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -20,5 +22,15 @@ public class UserHystrixMain {
     @LoadBalanced
     public RestTemplate initRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
